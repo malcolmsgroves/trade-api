a07fd5d039c513d5743d7ca0397ad94d5476b940
Mac Groves <mgroves@averoinc.com>
2019-07-14 16:46:12 -0600

    ignore build directory

--- build/env.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ENV = {
-    ALPACA_ENDPOINT: process.env.ALPACA_ENDPOINT || "",
-    APCA_API_KEY_ID: process.env.APCA_API_KEY_ID || "",
-    APCA_API_SECRET_KEY: process.env.APCA_API_SECRET_KEY || ""
-};
